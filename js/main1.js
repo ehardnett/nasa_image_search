30135a3eef537260693a9d6cea2798802ec8116a
erin <erin.hardnett.7@gmail.com>
2019-03-23 11:36:50 -0500

    faster loading

--- js/main1.js
+++ js/main1.js
@@ -66,6 +66,7 @@ function videoSearch() {
   }
 
   //load the appropriate refine search options for video: location and date
+  document.getElementById("search_options").style.visibility = "visible";
   document.getElementById("calendar").style.visibility = "visible";
   document.getElementById("size").style.visibility = "hidden";
   document.getElementById("locate").style.visibility = "visible";
@@ -172,10 +173,11 @@ function imageSearch() {
   }
 
   //sets the correct search options to be visible: all of them work for images
+  document.getElementById("search_options").style.visibility = "visible";
   document.getElementById("calendar").style.visibility = "visible";
   document.getElementById("size").style.visibility = "visible";
   document.getElementById("locate").style.visibility = "visible";
-  document.getElementById("rocket").style.visibility = "visible";
+  document.getElementById("rocket").style.visibility = "visible"; //loading has started
   currentPage = []; //reset current page array
 
   //obtain the inputs of the
@@ -197,7 +199,7 @@ function imageSearch() {
   Http.open("GET", url); // opens the api call to then parse through
   Http.send(); // sends the request to find any results
 
-  var dateFilter = checkDateRange(startInput, endInput);
+  var dateFilter = checkDateRange(startInput, endInput); // check to see if the inputs are correct
   if (dateFilter == null) {
     alert("Invaild Date Range");
   } else {
@@ -215,9 +217,11 @@ function imageSearch() {
           finalDescription = cutDescription(obj, i);
           locationStr = obj.collection.items[i].data[0].location;
           dateStr = obj.collection.items[i].data[0].date_created;
-          imageUrl = obj.collection.items[i].href;
+          imgUrl = obj.collection.items[i].href;
           nasaId = obj.collection.items[i].data[0].nasa_id;
+          imageTitles.push(title);
 
+          // reassigning undefined attributes of the image
           if (title == undefined){
             title = "No title available";
           }
@@ -234,32 +238,45 @@ function imageSearch() {
           image = document.createElement("img");
           image.setAttribute("src", obj.collection.items[i].links[0].href);
           image.setAttribute("alt", title);
-          if (sizeInput != "Select Size...") {
+          if (sizeInput != "Select Size...") { // check to see if a specific size is wanted by the user
             imageUrl = "http://images-assets.nasa.gov/image/" + encodeURIComponent(nasaId) + "/" + encodeURIComponent(nasaId) + "~" + sizeInput.toLowerCase() + ".jpg";
             document.getElementById("results").innerHTML += "<a href= " + encodeURI(imageUrl) + "><img id=thumbnail src=" + image.src + "></a><p>";
           } else {
+            // opens collection.json for the proper image url
             const img = new XMLHttpRequest();
-            imageJson = accessUrl(img, imageUrl);
-            if (imageJson != undefined) {
-              imgObj = JSON.parse(imageJson);
-              imageUrl = imgObj[0];
+            imageJson = accessUrl(img, imgUrl);
+            img.onreadystatechange=(e)=>{
+              var status = img.status;
+              if (status >= 200 && status <= 299){
+                if (imageJson != undefined) {
+                  imgObj = JSON.parse(imageJson);
+                  imageUrl = imgObj[0];
+                }
+              }
+            }
+            if (imageUrl != undefined) {
               document.getElementById("results").innerHTML += "<a href= " + encodeURI(imageUrl) + "><img id=thumbnail src=" + image.src + "></a><p>";
             } else {
               document.getElementById("results").innerHTML += "<img id=thumbnail onclick=unavailable(); src=" + image.src + "></a><p>";
             }
+            // creates the thumbnails for reoccurring titles
+            if (i < (obj.collection.items.length - 1)) {
+              var thumbStr = JSON.parse(addThumbnails(obj, i, JSON.stringify(imageTitles)));
+              thumbnails = thumbStr.thumbnails;
+            }
+
+            // generic info about the image
+            var info = {id: nasaId, pic: image.src, title: image.alt, describe: finalDescription, thumb: thumbnails, heart: "images/heart-gray.png", media: "image", location: locationStr, date: dateStr, url: imageUrl};
+            // info about the favorite image to be check with what is in localStorage
+            var liked = {id: nasaId, pic: image.src, title: image.alt, describe: finalDescription, thumb: thumbnails, heart: "images/heart-red.png", media: "image", location: locationStr, date: dateStr, url: imageUrl};
+
+            addToResults(JSON.stringify(info), JSON.stringify(liked));
+
+            i = thumbStr.j
+            }
           }
 
-
-          if (i < (obj.collection.items.length - 1)) {
-            thumbnails += addThumbnails(obj, i, imageUrl, imageTitles);
-          }
-
-          var info = {id: nasaId, pic: image.src, title: image.alt, describe: finalDescription, thumb: thumbnails, heart: "images/heart-gray.png", media: "image", location: locationStr, date: dateStr, url: imageUrl};
-          var liked = {id: nasaId, pic: image.src, title: image.alt, describe: finalDescription, thumb: thumbnails, heart: "images/heart-red.png", media: "image", location: locationStr, date: dateStr, url: imageUrl};
-
-          addToResults(JSON.stringify(info), JSON.stringify(liked));
-        }
-        document.getElementById("rocket").style.visibility = "hidden";
+        document.getElementById("rocket").style.visibility = "hidden"; //loading is complete
         inList = true;
         inGrid = false;
       } else {
@@ -281,10 +298,10 @@ function imageSearch() {
   - NOTE: Audio results didn't have a thumbnail to use in the JSON, so a simple audio thumbnail has been
   set for all results.
 */
-
 function audioSearch() {
   var searchInput, startInput, endInput;
 
+  // resets the inputs for the dates if the date type is not available
   if (dateAvail == false){
     document.getElementById("startDate").type = "text";
     document.getElementById("startDate").pattern = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d";
@@ -294,6 +311,8 @@ function audioSearch() {
     document.getElementById("endDate").title = "Use format mm/dd/yyyy";
   }
 
+  // sets the proper search options to be visible for an audio source
+  document.getElementById("search_options").style.visibility = "visible";
   document.getElementById("calendar").style.visibility = "visible";
   document.getElementById("size").style.visibility = "hidden";
   document.getElementById("locate").style.visibility = "hidden";
@@ -310,7 +329,7 @@ function audioSearch() {
   Http.open("GET", url); // opens the api call to then parse through
   Http.send(); // sends the request to find any results
 
-  var dateFilter = checkDateRange(startInput, endInput);
+  var dateFilter = checkDateRange(startInput, endInput); //checks to see if the dates are valid
   if (dateFilter == null) {
     alert("Invalid Date Range");
   } else {
@@ -329,6 +348,7 @@ function audioSearch() {
           audUrl = obj.collection.items[i].href;
           nasaId = obj.collection.items[i].data[0].nasa_id;
 
+          // reassigns attributes that we undefined
           if (title == undefined){
             title = "No title available";
           }
@@ -346,17 +366,26 @@ function audioSearch() {
           audio.setAttribute("src", "images/audio_thumbnail.png");
           audio.setAttribute("alt", title);
 
+          // accesses the collection.json for the audio source
           const aud = new XMLHttpRequest();
           audioJson = accessUrl(aud, audUrl);
-          if (audioJson != undefined) {
-            audObj = JSON.parse(audioJson);
-            audioUrl = audObj[0];
+          aud.onreadystatechange=(e)=>{
+            var status = aud.status;
+            if (status >= 200 && status <= 299){
+              if (audioJson != undefined) {
+                audObj = JSON.parse(audioJson);
+                audioUrl = audObj[0]; // pulling the correct url in the collection
+              }
+            }
+          }
+          if (audioUrl != undefined) {
             document.getElementById("results").innerHTML += "<a href= " + encodeURI(audioUrl) + "><img id=thumbnail src=" + audio.src + "></a><p>";
           } else {
             document.getElementById("results").innerHTML += "<img id=thumbnail onclick=unavailable(); src=" + audio.src + "></a><p>";
           }
-          console.log(audioUrl);
+          // generic information about the audio result
           var info = {id: JSON.stringify(nasaId), pic: audio.src, title: audio.alt, describe: finalDescription, heart: "images/heart-gray.png", media: "audio", thumb: "", location: locationStr, date: dateStr, url: audioUrl};
+          // information about the liked version of the result to see if it is in localStorage
           var liked = {id: JSON.stringify(nasaId), pic: audio.src, title: audio.alt, describe: finalDescription, heart: "images/heart-red.png", media: "audio", thumb: "", location: locationStr, date: dateStr, url: audioUrl};
 
           addToResults(JSON.stringify(info), JSON.stringify(liked));
@@ -391,10 +420,15 @@ function handleErrors(status) {
 }
 
 /*
-
+  accessUrl()
+  This helper function is used to access the collection.json where the correct image and
+  audio urls can be found. Only the ones that have an OK status will return a response.
+  Otherwise, it returns null and that source can't be accessed.
+    - Http: a new XMLHttpRequest for the url
+    - url: link to the collection.json for the source
 */
 function accessUrl(Http, url) {
-  Http.open("GET", url, false);
+  Http.open("GET", url, true);
   Http.send();
   if (Http.status >= 200 && Http.status <= 299) {
     return Http.responseText;
@@ -402,54 +436,115 @@ function accessUrl(Http, url) {
   return undefined;
 }
 
-function addThumbnails(obj, i, imageUrl, imageTitles) {
-  var str = "";
+/*
+  addThumbnails()
+  This helper function is used for recurring titles in an image search. To avoid having them
+  show up and look repetitive, this function takes all the images with the same title and creates a
+  thumbnails tag for the extra sources under the original. This returns an object with a string for
+  the thumbnails along with the most recent place in the items.
+    - obj: full json received from the search
+    - i: current place within the obj
+    - imageTitles: array with the previous titles of images in the search
+*/
+function addThumbnails(obj, i, imageTitles) {
+  var titles = JSON.parse(imageTitles);
+  var string = "";
   var j = i + 1;
   if (obj.collection.items[i].data[0].title != undefined && obj.collection.items[j].data[0].title != undefined) {
-    if (imageTitles[imageTitles.length - 1] == obj.collection.items[j].data[0].title) {
-      str += "More Thumbnails: ";
-      while (imageTitles[imageTitles.length - 1] == obj.collection.items[j].data[0].title) {
-        str += "<a id=more href="+ imageUrl +">Source " + (j - i) + "</a> ";
+    if (titles[titles.length - 1] == obj.collection.items[j].data[0].title) {
+      string += "More Thumbnails: ";
+      while (titles[titles.length - 1] == obj.collection.items[j].data[0].title) {
+        //access the new url in the collection.json
+        const img = new XMLHttpRequest();
+        var imageUrl = obj.collection.items[j].href;
+        var imageJson = accessUrl(img, imageUrl);
+        img.onreadystatechange=(e)=>{
+          var status = img.status;
+          if (status >= 200 && status <= 299){
+            if (imageJson != undefined) {
+              console.log("access");
+              imgObj = JSON.parse(imageJson);
+              imageUrl = imgObj[0];
+            }
+          }
+        }
+        var finalDescription = cutDescription(obj, j);
+        var info = {id: obj.collection.items[j].data[0].title, pic: obj.collection.items[j].links[0].href, title: obj.collection.items[j].data[0].title, describe: finalDescription, thumb: "", heart: "images/heart-gray.png", media: "image",
+        location: obj.collection.items[j].data[0].location, date: obj.collection.items[j].data[0].date_created, url: imageUrl};
+        var liked = {id: obj.collection.items[j].data[0].title, pic: obj.collection.items[j].links[0].href, title: obj.collection.items[j].data[0].title, describe: finalDescription, thumb: "", heart: "images/heart-heart.png", media: "image",
+        location: obj.collection.items[j].data[0].location, date: obj.collection.items[j].data[0].date_created, url: imageUrl};
+        if (localAvail == true) {
+          var inStorage = JSON.parse(localStorage.getItem("list"));
+          if (inStorage != null) {
+            var index = checkStorage(JSON.stringify(info), JSON.stringify(inStorage));
+            if (index == -1) {
+              currentPage.push(info);
+            } else {
+              currentPage.push(liked);
+            }
+          }
+        }
+        //create thumbnail sources and add to the initial string
+        if (imageUrl != undefined) {
+          console.log("thumbnail");
+          string += "<a id=more href=" + encodeURI(imageUrl) +">Source " + (j - i) + "</a>  ";
+        } else {
+          console.log("no thumbnail");
+          string += "<a id=more onclick=unavailable()>Source " + (j - i) + "</a>  ";
+        }
         j++;
         if (obj.collection.items[j].data[0].title == undefined) {
           break;
         }
       }
-      i = j;
     }
   }
+  var str = {thumbnails: string, j: j};
 
-  return str;
+  return JSON.stringify(str);
 }
 
+/*
+  addToResults()
+  This function is used to add the information collected from the original search. All
+  the information is displayed on the screen in the "results" divider. At each checkpoint,
+  the code is checking to see if there is an instance of the result already in either
+  sessionStorage or localStorage that differs from the generic info object.
+    - info: object holding all the information for the result (no favorite)
+    - liked: object holding all the information for the result (favorite)
+*/
 function addToResults(info, liked) {
   var information = JSON.parse(info);
   var likeInfo = JSON.parse(liked);
   if (localAvail == true) {
     var inStorage = JSON.parse(localStorage.getItem("list"));
     if (inStorage != null) {
-      var index = checkStorage(JSON.stringify(information), JSON.stringify(inStorage));
+      var index = checkStorage(JSON.stringify(information), JSON.stringify(inStorage)); // check for if it is a favorite
       if (sessionAvail == true) {
         if (index == -1) {
-          sessionStorage.setItem(information.id, JSON.stringify(information));
+          sessionStorage.setItem(information.id, JSON.stringify(information)); //store generic information
           addToIndex(JSON.stringify(information));
         } else {
-          sessionStorage.setItem(likeInfo.id, JSON.stringify(likeInfo));
+          sessionStorage.setItem(likeInfo.id, JSON.stringify(likeInfo)); // store liked information
           addToIndex(JSON.stringify(likeInfo));
         }
       } else {
         addToIndex(JSON.stringify(information));
       }
     } else {
-      sessionStorage.setItem(information.id, JSON.stringify(information));
+      sessionStorage.setItem(information.id, JSON.stringify(information)); //store generic information
       addToIndex(JSON.stringify(information));
     }
-  } else {
+  } else { //no storage allowed then added to currentPage without heart
     document.getElementById("results").innerHTML += "<div class=info><strong>" + information.title + "</strong><p><i>" + information.describe + "</i></p>";
     currentPage.push(information);
   }
 }
 
+/*
+  checkStorage()
+  This function is used to
+*/
 function checkStorage(info, inStorage) {
   var stored = JSON.parse(info);
   var storage = JSON.parse(inStorage);
@@ -465,7 +560,11 @@ function checkStorage(info, inStorage) {
 
 function addToIndex(info) {
   var arr = JSON.parse(info);
-  document.getElementById("results").innerHTML += "<div class=info><strong>" + arr.title + "</strong><p><i>" + arr.describe + "</i><p><img id=" + arr.id +" class=heart src=\"" + arr.heart +"\" onclick=addToFavorites(\"" + arr.id + "\");></p>";
+  if (arr.thumb != undefined) {
+    document.getElementById("results").innerHTML += "<div class=info><strong>" + arr.title + "</strong><p><i>" + arr.describe + "</i><p>" + arr.thumb +"<p><img id=" + arr.id +" class=heart src=\"" + arr.heart +"\" onclick=addToFavorites(\"" + JSON.stringify(arr.id) + "\");></p>";
+  } else {
+    document.getElementById("results").innerHTML += "<div class=info><strong>" + arr.title + "</strong><p><i>" + arr.describe + "</i><p><img id=" + arr.id +" class=heart src=\"" + arr.heart +"\" onclick=addToFavorites(\"" + JSON.stringify(arr.id) + "\");></p>";
+  }
   currentPage.push(arr);
 }
 
@@ -532,7 +631,8 @@ function unavailable() {
 }
 
 function addToFavorites(nasaId){
-  var info = sessionStorage.getItem(nasaId);
+  var id = JSON.parse(nasaId);
+  var info = sessionStorage.getItem(id);
   if (localStorage.getItem("list") != null) {
     favoriteImgs = JSON.parse(localStorage.getItem("list"));
   }
@@ -579,7 +679,8 @@ function displayGrid() {
     for (var i = 0; i < currentPage.length; i++) {
       var info = currentPage[i];
       var newTitle = changeTitle(info.title);
-      document.getElementById("results").innerHTML += "<div class=gridView <a href= " + encodeURI(info.url) + "><img id=grid src=" + info.pic + "></a><strong>" + newTitle + " </strong><img id=" + info.id + " class=heart src=" + info.heart + " onclick=addToFavorites(\'" + info.id +"\')>";
+      document.getElementById("results").innerHTML += "<div class=gridView <a href= " + encodeURI(info.url) + "><img id=grid src=" + info.pic + "></a><strong>" + newTitle + " </strong><img id=" + info.id + " class=heart src=" + info.heart + " onclick=addToFavorites(\'" + JSON.stringify(info.id) +"\')>";
+
     }
     document.getElementById("rocket").style.visibility = "hidden";
     inGrid = true;
@@ -609,7 +710,7 @@ function displayList() {
     for (var i = 0; i < currentPage.length; i++) {
       var info = currentPage[i];
       document.getElementById("results").innerHTML += "<a href= " + encodeURI(info.url) + "><img id=thumbnail src=" + info.pic + "></a><p>";
-      document.getElementById("results").innerHTML += "<div class=info><strong>" + info.title + "</strong><p><i>" + info.describe + "</i><p><img id=" + info.id +" class=heart src=\"" + info.heart +"\" onclick=addToFavorites(\"" + info.id + "\");></p>";
+      document.getElementById("results").innerHTML += "<div class=info><strong>" + info.title + "</strong><p><i>" + info.describe + "</i><p><img id=" + info.id +" class=heart src=\"" + info.heart +"\" onclick=addToFavorites(\"" + JSON.stringify(info.id) + "\");></p>";
     }
     document.getElementById("rocket").style.visibility = "hidden";
     inGrid = false;
@@ -649,7 +750,7 @@ function findcurrentPage() {
         currentPage[i].url = "http://images-assets.nasa.gov/image/"+ currentPage[i].id + "/" + currentPage[i].id + "~" + sizeInput.toLowerCase() +".jpg"
       }
     }
-    
+
     if (inGrid == true) {
       displayGrid();
     } else {
